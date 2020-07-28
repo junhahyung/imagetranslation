b58780e5e62cca3a75b6f71761dc4d07d95efbde
Junha <sharpeeee@naver.com>
2020-07-28 22:07:01 +0900

    first commit

---
+++ train.py
@@ -0,0 +1,89 @@
+import os
+import sys
+import shutil
+import argparse
+
+import torch
+import tensorboardx
+import torch.backends.cudnn as cudnn
+
+from torch.autograd import Variable
+
+from trainer import MUNIT_Trainer, UNIT_Trainer
+from utils import get_all_data_loaders, prepare_sub_folder, write_html, write_loss, get_config, write_2images, Timer
+try:
+    from itertools import izip as zip
+except ImportError:
+    pass
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--config', type=str, default='configs/edges2handbags_folder.yaml', help='Path to the config file.')
+parser.add_argument('--output_path', type=str, default='.', help='outputs path')
+parser.add_argument('--resume', action='store_true')
+parser.add_argument('--trainer', type=str, default='MUNIT', help='MUNIT|UNIT')
+opts = parser.parse_args()
+
+# enables faster runtime when input does not vary
+cudnn.benchmark = True
+
+config = get_config(opts.config)
+max_iter = config['max_iter']
+display_size = config['display_size']
+config['vgg_model_path'] = opts.output_path
+
+
+if opts.trainer == 'MUNIT':
+    trainer = MUNIT_Trainer(config)
+elif opts.trainer == 'UNIT':
+    trainer = UNIT_Trainer(config)
+else:
+    sys.exit("Only support MUNIT|UNIT")
+
+trainer.cuda()
+train_loader_a, train_loader_b, test_loader_a, test_loader_b = get_all_data_loaders(config)
+train_display_images_a = torch.stack([train_loader_a.dataset[i] for i in range(display_size)]).cuda()
+train_display_images_b = torch.stack([train_loader_b.dataset[i] for i in range(display_size)]).cuda()
+test_display_images_a = torch.stack([test_loader_a.dataset[i] for i in range(display_size)]).cuda()
+test_display_images_b = torch.stack([test_loader_b.dataset[i] for i in range(display_size)]).cuda()
+
+model_name = os.path.splitext(os.path.basename(opts.config))[0]
+train_writer = tensorboardx.SummaryWriter(os.path.join(opts.output_path + "/logs", model_name))
+output_directory = os.path.join(opts.output_path + "/outputs", model_name)
+checkpoint_directory, image_directory = prepare_sub_folder(output_directory)
+shutil.copy(opts.config, os.path.join(output_directory, 'config.yaml'))
+
+# training
+iterations = trainer.resume(checkpoint_directory, hyperparameters=config) if opts.resume else 0
+while True:
+    for it, (images_a, images_b) in enumerate(zip(train_loader_a, train_loader_b)):
+        trainer.update_learning_rate()
+        images_a, images_b = images_a.cuda().detach(), images_b.cuda().detach()
+
+        with Timer("Elapsed time in update: %f"):
+            trainer.dis_update(images_a, images_b, config)
+            trainer.gen_update(images_a, images_b, config)
+
+            # waits for all kernels in all streams on a CUDA device to complete ??
+            torch.cuda.synchronize()
+
+        if (iterations + 1) % config['log_iter'] == 0:
+            print("Iteration: %08d/%08d" % (iterations + 1, max_iter))
+            write_loss(iterations, trainer, train_writer)
+        
+        if (iterations + 1) % config['image_save_iter'] == 0:
+            with torch.no_grad():
+                test_image_outputs = trainer.sample(test_display_images_a, test_display_images_b)
+                train_image_outputs = trainer.sample(train_display_images_a, train_display_images_b)
+            write_2images(test_image_outputs, display_size, image_directory, 'test_%08d' % (iterations + 1))
+            write_2images(train_image_outputs, display_size, image_directory, 'train_%08d' % (iterations + 1))
+            # HTML
+            write_html(output_directory + "/index.html", iterations + 1, config['image_save_iter'], 'images')
+        
+        # Save network weights
+        if (iterations + 1) % config['snapshot_save_iter'] == 0:
+            trainer.save(checkpoint_directory, iterations)
+        
+        iterations += 1
+        if iterations >= max_iter:
+            sys.exit('Finish training')
